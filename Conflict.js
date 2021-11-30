369910ce12d767aa6de5136cf08c1505fcca8fa8
yusufggur <92791302+yusufggur@users.noreply.github.com>
2021-11-30 17:54:40 +0300

    Update Conflict.js

--- Conflict.js
+++ Conflict.js
@@ -2,3 +2,9 @@ function topla(let sayi1, let sayi2){
 sonuc = sayi1 + sayi2
 console.log("Toplamı = " sonuc)
 }
+
+
+function cikart(let sayi1, let sayi2){
+sonuc = sayi1 - sayi2
+console.log("<Çıkart = " sonuc)
+}
