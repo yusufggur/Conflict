9a03227cb99569d47eb83cc1b5dad5618b0d0d9b
TurhanCetin <turhancetin30@mail.com>
2021-11-30 18:18:50 +0300

    Hatalar Düzeltildi

--- Conflict.js
+++ Conflict.js
@@ -1,10 +1,15 @@
-function topla(let sayi1, let sayi2){
+let sayi1
+let sayi2
+let sonuc
+
+
+function topla(sayi1,sayi2,sonuc){
 sonuc = sayi1 + sayi2
-console.log("Toplamı = " sonuc)
+console.log("Toplamı = " + sonuc)
 }
 
 
-function cikart(let sayi1, let sayi2){
+function cikart( sayi1, sayi2, sonuc){
 sonuc = sayi1 - sayi2
-console.log("<Çıkart = " sonuc)
+console.log("<Çıkart = " + sonuc)
 }
